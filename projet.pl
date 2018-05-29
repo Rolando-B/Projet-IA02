f746f285e676cc54faf3c5bbd83daa28e17a406a
Bastien ROLANDO <bastien.rolando@gmail.com>
2018-05-29 14:04:29 +0200

    workign

--- projet.pl
+++ projet.pl
@@ -11,23 +11,24 @@ imprimeGrille([T|Q],I) :- 0 is mod(I,3),imprimeListe(T,1),nl, write('- - - - - -
 imprimeGrille([T|Q],I) :- imprimeListe(T,1),nl, N is I+1, imprimeGrille(Q,N).
 
 imprimeListe([],I).
-imprimeListe([T|Q],I) :- 0 is mod(I,3), write(T),write('|'), N is I+1, imprimeListe(Q,N).
+imprimeListe([T|Q],I) :- 0 is mod(I,3), write(T), write('|'), N is I+1, imprimeListe(Q,N).
 imprimeListe([T|Q],I) :- write(T), N is I+1, imprimeListe(Q,N).
 
 concat([],L,L).
 concat([T|Q],L,[T|R]) :- concat(Q,L,R).
 
-recupLigne([H|_],1,H) :- !.
-recupLigne([_|T],N,H) :- N > 0, N1 is N-1, recupLigne(T,N1,H).
 
 inverse([], []).
 inverse([F|Fs], T) :- inverse(F, [F|Fs], T).
-
 inverse([], _, []).
 inverse([_|Res], L, [T|Q]) :- restList(L, T, L1), inverse(Res, L1, Q).
 
 restList([], [], []).
 restList([[F|Os]|Rest], [F|Fs], [Os|Oss]) :- restList(Rest, Fs, Oss).
+
+
+recupLigne([H|_],1,H) :- !.
+recupLigne([_|T],N,H) :- N > 0, N1 is N-1, recupLigne(T,N1,H).
 
 recupColonne(L,I,H) :- inverse(L,X), recupLigne(X,I,H).
 
