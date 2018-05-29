15b57011af6c0b48602c7a88ef1077d2a83b9f87
Bastien ROLANDO <bastien.rolando@gmail.com>
2018-05-29 09:10:22 +0200

    sous listes done

--- projet.pl
+++ projet.pl
@@ -1,27 +1,25 @@
 %change_directory('Z:/ia02').
 %consult('projet').
 
-liste(L):-L=[o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o].
+liste(L):-L=[[o,o,o,o,o,o,o,o,o],[o,o,o,o,o,o,o,o,o],[o,o,o,o,o,o,o,o,o],[o,o,o,o,o,o,o,o,o],[o,o,o,o,o,o,o,o,o],[o,o,o,o,o,o,o,o,o],[o,o,o,o,o,o,o,o,o],[o,o,o,o,o,o,o,o,o],[o,o,o,o,o,o,o,o,o]].
 
-disp(T) :- imprime(T,1).
+imprime(T) :- imprimeGrille(T,1).
 
-imprime([],I).
-imprime([T|Q],I) :- 0 is mod(I,27), write(T),nl, write('- - - - - -'),nl, N is I+1, imprime(Q,N).
-imprime([T|Q],I) :- 0 is mod(I,9), write(T), nl, N is I+1, imprime(Q,N).
-imprime([T|Q],I) :- 0 is mod(I,3), write(T),write('|'), N is I+1, imprime(Q,N).
-imprime([T|Q],I) :- write(T), N is I+1, imprime(Q,N).
+
+imprimeGrille([],I).
+imprimeGrille([T|Q],I) :- 0 is mod(I,3),imprimeListe(T,1),nl, write('- - - - - -'),nl, N is I+1, imprimeGrille(Q,N).
+imprimeGrille([T|Q],I) :- imprimeListe(T,1),nl, N is I+1, imprimeGrille(Q,N).
+
+imprimeListe([],I).
+imprimeListe([T|Q],I) :- 0 is mod(I,3), write(T),write('|'), N is I+1, imprimeListe(Q,N).
+imprimeListe([T|Q],I) :- write(T), N is I+1, imprimeListe(Q,N).
 
 concat([],L,L).
 concat([T|Q],L,[T|R]) :- concat(Q,L,R).
 
-recupLigne([],N,R).
-recupLigne(L,N,R) :- ligne(L,1,1,N,R).
+ligne(L,N,R) :- recupLigne(L,N,R,1).
+
+recupLigne([T|Q],N,L,I) :- write(N), write(I), nl, N=I, recupLigne(Q,N,T,A), !.
+recupLigne([T|Q],N,L,I) :- write(N), write(I), nl, A is I+1, recupLigne(Q,N,L,A).
 
 
-
-
-ligne([],I,J,K,R).
-ligne([T|Q],I,J,K,R) :- 0 is mod(I,9),I2 is I+1,J2 is J+1, ligne(Q,I2,J2,K,R).
-ligne([T|Q],I,J,K,R) :- J =:= K,concat(T,R,R), I2 is I+1, ligne(Q,I2,J,K,R).
-ligne([T|Q],I,J,K,R) :- I2 is I+1,ligne(Q,I2,J,K,R).
-ligne([T|Q],I,J,K,R) :- J > K, !, fail.
