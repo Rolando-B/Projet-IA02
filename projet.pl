4ad7c1e81d66d470a41eef96738548e29809b142
Bastien ROLANDO <bastien.rolando@gmail.com>
2018-05-30 17:04:38 +0200

    logical way working

--- projet.pl
+++ projet.pl
@@ -18,10 +18,10 @@ element(X,[_|Q]) :- element(X,Q).
 
 unique([]).
 unique([_,[]]).
-unique([T,Q]) :- var(T), unique(Q),!.
+unique([T,Q]) :- var(T), unique(Q), !.
 unique([T|Q]) :- \+element(T,Q),unique(Q).
 
-puzzle(L) :- L =[
+grille(L) :- L =[
     S11,S12,S13,S14,S15,S16,S17,S18,S19,
     S21,S22,S23,S24,S25,S26,S27,S28,S29,
     S31,S32,S33,S34,S35,S36,S37,S38,S39,
