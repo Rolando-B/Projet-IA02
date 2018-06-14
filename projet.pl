330cbfaa31f6a78147b106f0ff4af40df585e309
Bastien ROLANDO <bastien.rolando@gmail.com>
2018-06-14 10:03:11 +0200

    menu v2

--- projet.pl
+++ projet.pl
@@ -1,6 +1,6 @@
 %change_directory('Z:/ia02').
 %consult('projet').
-
+%------------------------------------Grille de Test
 liste2(L):- L=[
     _,_,5,3,_,_,_,_,_,
     8,_,_,_,_,_,_,2,_,
@@ -11,6 +11,41 @@ liste2(L):- L=[
     _,6,_,5,_,_,_,_,9,
     _,_,4,_,_,_,_,3,_,
     _,_,_,_,_,9,7,_,_
+  ].
+  listeVide(L):- L=[
+    _,_,_,_,_,_,_,_,_,
+    _,_,_,_,_,_,_,_,_,
+    _,_,_,_,_,_,_,_,_,
+    _,_,_,_,_,_,_,_,_,
+    _,_,_,_,_,_,_,_,_,
+    _,_,_,_,_,_,_,_,_,
+    _,_,_,_,_,_,_,_,_,
+    _,_,_,_,_,_,_,_,_,
+    _,_,_,_,_,_,_,_,_
+  ].
+
+  liste3(A):- A=[
+  1,2,_,4,5,6,7,8,9,
+  4,5,6,7,_,9,1,_,3,
+  7,8,9,1,2,3,4,5,_,
+  2,_,4,5,6,7,8,9,1,
+  5,6,7,_,9,1,2,3,4,
+  8,9,1,2,3,4,5,6,7,
+  3,4,5,6,7,8,9,1,2,
+  _,7,_,9,1,2,_,4,5,
+  9,1,2,3,4,5,6,7,8
+  ].
+
+  liste4(A):- A=[
+  1,2,_,4,5,6,7,_,9,
+  4,_,_,_,8,_,1,2,3,
+  7,_,9,1,2,3,_,5,_,
+  2,3,_,5,_,7,8,_,1,
+  5,_,7,_,9,1,2,_,4,
+  8,_,1,_,_,_,5,6,7,
+  3,_,_,6,7,_,9,_,2,
+  6,_,8,9,1,_,3,_,5,
+  9,1,2,3,_,5,6,7,8
   ].
 
 
@@ -26,6 +61,28 @@ liste2(L):- L=[
   9,1,2,3,4,5,6,7,8
   ].
 
+concat([],L,L).
+concat([T|Q],L,[T|R]) :- concat(Q,L,R).
+
+element(X,[X|_],0).
+element(X,[_|Q],R) :- element(X,Q,N), R is N+1.
+
+element(X,L) :- element(X,L,_).
+
+replace([_|T], 0, X, [X|T]).
+replace([H|T], I, X, [H|R]):- I > -1, NI is I-1, replace(T, NI, X, R), !.
+replace(L, _, _, L).
+
+generer(A, [A|B], B).
+generer(A, [B|C], [B|D]) :- generer(A,C,D).
+
+valeur(X):- generer(X,[1,2,3,4,5,6,7,8,9],_). %capable de générer un X qui prend des valeurs de 1 à 9
+
+permute([], []).
+permute([X|Rest], L) :-
+    permute(Rest, L1),
+    select(X, L, L1).
+
 
 pleine([]).
 pleine([T|Q]) :- \+ var(T), pleine(Q).
@@ -36,12 +93,127 @@ unique([]).
 unique([T|Q]) :- maplist(dif(T), Q), unique(Q).
 
 domaine([]).
-domaine([T|Q]) :- var(T), domaine(Q).
+domaine([T|Q]) :- var(T), domaine(Q), !.
 domaine([T|Q]) :- T<10, T>0, domaine(Q).
 
-etatFinal(L) :- grille(L), pleine(L).
+etatFinal(L) :- valide(L), pleine(L).
 
-grille(L) :- L =[
+
+%--------------------------Tentative pour générer une grille résolvable--------------------------
+
+%générer une liste de nombre allant de 1 à 9
+genere_liste(0,[]):- !.
+genere_liste(UB, [UB|D]):- N is UB - 1, genere_liste(N,D).
+
+liste_1_9(L):- genere_liste(9,L).
+
+%retirer un élément d'une liste
+retire_el([],_,[]).
+retire_el([X|Q],X,Q):-!.
+retire_el([T|Q],X,[T|D]):- retire_el(Q,X,D).
+
+%générer une Ligne de longueur N (Nous mettrons 9 en longueur pour chacune de nos lignes)
+genere_ligne(0,L,[],[]):-!.
+genere_ligne(N,ListeNbs,[X|L],NewListeNbs2):- element(X,ListeNbs), retire_el(ListeNbs,X,NewListeNbs),
+                                              M is N - 1 ,genere_ligne(M, NewListeNbs, L, NewListeNbs2).
+
+
+%Générer une grille de sudoku pleine
+/*grille(N,C):- UB is N*N, liste_1_9(Nbs), grille(N,C,Nbs).
+
+grille(_,[],[]).
+grille(N,[L|C],Nbs):- genere_ligne(N,Nbs,L,NewNbs), grille(N,C,NewNbs).*/
+
+add(X, L, [X|L]).
+add_list([], L, L).
+add_list([T|Q], L, L1) :- add(T, L2, L1), add_list(Q, L, L2).
+
+grille(0,[]):-!.
+grille(N,Res):- liste_1_9(Nbs), genere_ligne(N,Nbs,L,NewNbs), add_list(L,Res,R),
+                M is N - 1, solve(R,R2), grille(M,R2).
+
+/*Le début fonctionne mais impossible de concaténer 9 listes pour l'instant mais avec les prédicats
+en console je peux en concaténer deux */
+
+
+%----------------------------------------sudoku player
+sudoku :- nl,
+  write('------------------------------------------------------------------'),nl,
+  write('======= Programme Sudoku - Projet IA02 - Touzeau / Rolando ======='),nl,
+  write('-------------------------------------------------------------------'),nl,nl,
+  repeat, menu, !.
+menu :- write('\t\t=====  MENU  ====='),nl,nl,
+  write(' Que voulez vous faire ?'),nl,nl,
+  write('1. Resoudre un sudoku'),nl,
+  write('2. Proposer un sudoku'),nl,
+  write('3. Quitter'),nl,nl,
+  write('Que voulez-vous faire ? : '),
+  read(Choice), nl,
+  gestion_choix(Choice),
+  Choice=3.
+
+gestion_choix(1):- write('---- RESOLUTION D\'UN SUDOKU ----'),nl,
+            liste3(S),
+            asserta(sudokuSave(S)),
+            repeat,
+            userSolvingSudoku, !.
+
+userSolvingSudoku :- nl,write('---- Complétez le sudoku ----'),nl,nl,
+   sudokuSave(S), disp(S), nl,
+  write('1. Definir numero'), nl,
+  write('2. Effacer numero'), nl,
+  write('3. Résoudre'), nl,
+  write('4. Quitter'), nl, nl,
+  write('Que voulez-vous faire ? : '),
+  read(Choice), nl,
+  fillSudoku(Choice,S),
+  sudokuSave(ModifiedGrid),
+  fail.
+% ------------------------------------ Gestion user
+
+isPlayableCell(N,Sudoku):- element(X,Sudoku,N), var(X),!.
+isPlayableCell(_,_):- write('Cette case n\'est pas jouable!'),nl,fail.
+
+validUserInputNumber(X):- X>0, X=<9, integer(X), !.
+validUserInputNumber(_):- nl, write('Saisie incorrecte !'),nl,nl, fail.
+
+fillSudoku(1,S):- write('Ligne de la case à modifier : '), read(X), validUserInputNumber(X),nl,
+  write('Colonne de la case à modifier :'), read(Y), validUserInputNumber(Y),nl,
+  write('Valeur de la case : '), read(Z), validUserInputNumber(Z),nl,
+  I is ((X-1)*9)+Y,
+  isPlayableCell(I,S),
+  N is I-1,
+  replace(S,N,Z,S1),
+  retract(sudokuSave(S)),
+  asserta(sudokuSave(S1)),
+  write('Succès de l\'ajout.'),nl.
+
+fillSudoku(1,_):- !.
+
+fillSudoku(2,S):- write('Ligne de la case à effacer :'), read(X), validUserInputNumber(X),nl,
+  write('Colonne de la case à effacer:'), read(Y), validUserInputNumber(Y),nl,
+  I is ((X-1)*9)+Y,
+  isPlayableCell(I,S),
+  replace(S,I,_,S1),
+  retract(sudokuSave(S)),
+  asserta(sudokuSave(S1)).
+
+fillSudoku(2,_):- !.
+
+fillSudoku(3,S):-
+  solve(S,S1),
+  retract(sudokuSave(S)),
+  asserta(sudokuSave(S1)).
+
+fillSudoku(3,_):- !.
+
+handleResolution(4,_):-!.
+
+
+%------------------------------------- Résolution d'un sudoku
+solve(Puzzle,Solution) :-
+Solution = Puzzle,
+Puzzle =[
     S11,S12,S13,S14,S15,S16,S17,S18,S19,
     S21,S22,S23,S24,S25,S26,S27,S28,S29,
     S31,S32,S33,S34,S35,S36,S37,S38,S39,
@@ -51,49 +223,50 @@ grille(L) :- L =[
     S71,S72,S73,S74,S75,S76,S77,S78,S79,
     S81,S82,S83,S84,S85,S86,S87,S88,S89,
     S91,S92,S93,S94,S95,S96,S97,S98,S99
-],
-/* Test du domaine */
-domaine(L),
-/* ligne */
-unique([S11,S12,S13,S14,S15,S16,S17,S18,S19]),
-unique([S21,S22,S23,S24,S25,S26,S27,S28,S29]),
-unique([S31,S32,S33,S34,S35,S36,S37,S38,S39]),
-unique([S41,S42,S43,S44,S45,S46,S47,S48,S49]),
-unique([S51,S52,S53,S54,S55,S56,S57,S58,S59]),
-unique([S61,S62,S63,S64,S65,S66,S67,S68,S69]),
-unique([S71,S72,S73,S74,S75,S76,S77,S78,S79]),
-unique([S81,S82,S83,S84,S85,S86,S87,S88,S89]),
-unique([S91,S92,S93,S94,S95,S96,S97,S98,S99]),
-/* colonne */
-unique([S11,S21,S31,S41,S51,S61,S71,S81,S91]),
-unique([S12,S22,S32,S42,S52,S62,S72,S82,S92]),
-unique([S13,S23,S33,S43,S53,S63,S73,S83,S93]),
-unique([S14,S24,S34,S44,S54,S64,S74,S84,S94]),
-unique([S15,S25,S35,S45,S55,S65,S75,S85,S95]),
-unique([S16,S26,S36,S46,S56,S66,S76,S86,S96]),
-unique([S17,S27,S37,S47,S57,S67,S77,S87,S97]),
-unique([S18,S28,S38,S48,S58,S68,S78,S88,S98]),
-unique([S19,S29,S39,S49,S59,S69,S79,S89,S99]),
-/* carré */
-unique([S11,S12,S13,S21,S22,S23,S31,S32,S33]),
-unique([S14,S15,S16,S24,S25,S26,S34,S35,S36]),
-unique([S17,S18,S19,S27,S28,S29,S37,S38,S39]),
-unique([S41,S42,S43,S51,S52,S53,S61,S62,S63]),
-unique([S44,S45,S46,S54,S55,S56,S64,S65,S66]),
-unique([S47,S48,S49,S57,S58,S59,S67,S68,S69]),
-unique([S71,S72,S73,S81,S82,S83,S91,S92,S93]),
-unique([S74,S75,S76,S84,S85,S86,S94,S95,S96]),
-unique([S77,S78,S79,S87,S88,S89,S97,S98,S99]).
+  ],
+  /* ligne */
+  Ligne1 = [S11,S12,S13,S14,S15,S16,S17,S18,S19],
+  Ligne2 = [S21,S22,S23,S24,S25,S26,S27,S28,S29],
+  Ligne3 = [S31,S32,S33,S34,S35,S36,S37,S38,S39],
+  Ligne4 = [S41,S42,S43,S44,S45,S46,S47,S48,S49],
+  Ligne5 = [S51,S52,S53,S54,S55,S56,S57,S58,S59],
+  Ligne6 = [S61,S62,S63,S64,S65,S66,S67,S68,S69],
+  Ligne7 = [S71,S72,S73,S74,S75,S76,S77,S78,S79],
+  Ligne8 = [S81,S82,S83,S84,S85,S86,S87,S88,S89],
+  Ligne9 = [S91,S92,S93,S94,S95,S96,S97,S98,S99],
+  /* colonne */
+  Colonne1 = [S11,S21,S31,S41,S51,S61,S71,S81,S91],
+  Colonne2 = [S12,S22,S32,S42,S52,S62,S72,S82,S92],
+  Colonne3 = [S13,S23,S33,S43,S53,S63,S73,S83,S93],
+  Colonne4 = [S14,S24,S34,S44,S54,S64,S74,S84,S94],
+  Colonne5 = [S15,S25,S35,S45,S55,S65,S75,S85,S95],
+  Colonne6 = [S16,S26,S36,S46,S56,S66,S76,S86,S96],
+  Colonne7 = [S17,S27,S37,S47,S57,S67,S77,S87,S97],
+  Colonne8 = [S18,S28,S38,S48,S58,S68,S78,S88,S98],
+  Colonne9 = [S19,S29,S39,S49,S59,S69,S79,S89,S99],
+  /* carré */
+  Carre1 = [S11,S12,S13,S21,S22,S23,S31,S32,S33],
+  Carre2 = [S14,S15,S16,S24,S25,S26,S34,S35,S36],
+  Carre3 = [S17,S18,S19,S27,S28,S29,S37,S38,S39],
+  Carre4 = [S41,S42,S43,S51,S52,S53,S61,S62,S63],
+  Carre5 = [S44,S45,S46,S54,S55,S56,S64,S65,S66],
+  Carre6 = [S47,S48,S49,S57,S58,S59,S67,S68,S69],
+  Carre7 = [S71,S72,S73,S81,S82,S83,S91,S92,S93],
+  Carre8 = [S74,S75,S76,S84,S85,S86,S94,S95,S96],
+  Carre9 = [S77,S78,S79,S87,S88,S89,S97,S98,S99],
+
+  Sets = [Ligne1,Ligne2,Ligne3,Ligne4,Ligne5,Ligne6,Ligne7,Ligne8,Ligne9,
+          Colonne1,Colonne2,Colonne3,Colonne4,Colonne5,Colonne6,Colonne7,Colonne8,Colonne9,
+          Carre1,Carre2,Carre3,Carre4,Carre5,Carre6,Carre7,Carre8,Carre9],
+  maplist(permute([1,2,3,4,5,6,7,8,9]), Sets).
 
 disp(T) :- imprime(T,1).
 
-imprime([],_).
-imprime([T|Q],I) :- 0 is mod(I,27), I \= 81, writeCase(T),nl, write('= = = = = = = = = = = = = = ='),nl, N is I+1, imprime(Q,N).
-imprime([T|Q],I) :- 0 is mod(I,9), writeCase(T), nl, N is I+1, imprime(Q,N).
-imprime([T|Q],I) :- 0 is mod(I,3), writeCase(T),write('|'), N is I+1, imprime(Q,N).
-imprime([T|Q],I) :- writeCase(T), N is I+1, imprime(Q,N).
+imprime([],_):- !.
+imprime([T|Q],I) :- 0 is mod(I,27), I \= 81, writeCase(T),nl, write('= = = = = = = = = = = = = = ='),nl, N is I+1, imprime(Q,N),!.
+imprime([T|Q],I) :- 0 is mod(I,9), writeCase(T), nl, N is I+1, imprime(Q,N),!.
+imprime([T|Q],I) :- 0 is mod(I,3), writeCase(T),write('|'), N is I+1, imprime(Q,N),!.
+imprime([T|Q],I) :- writeCase(T), N is I+1, imprime(Q,N),!.
 
 writeCase(X) :- var(X), write(' - '),!.
 writeCase(X) :- write(' '),write(X),write(' ').
-
-
---
+++ test.pl
@@ -0,0 +1,300 @@
+
+              %%% SUDOKU-SUDOKU-SUDOKU %%%
+
+
+  %% ---AUTRES--- %%
+
+sudokuInitial([[[[9,' ',8],[' ',1,7],[' ',5,' ']],
+      [[' ',' ',' '],[5,9,' '],[7,' ',8]],
+      [[' ',' ',4],[6,' ',' '],[' ',' ',' ']]],
+      [[[' ',' ',' '],[' ',7,' '],[2,8,1]],
+      [[' ',7,3],[9,' ',8],[5,6,' ']],
+      [[2,8,6],[' ',4,' '],[' ',' ',' ']]],
+      [[[' ',' ',' '],[' ',' ',4],[9,' ',' ']],
+      [[3,' ',7],[' ',6,9],[' ',' ',' ']],
+      [[' ',6,' '],[8,3,' '],[4,' ',2]]]]).
+
+sudokuTest([[[[9,2,8],[4,1,7],[6,5,3]],
+      [[6,3,1],[5,9,2],[7,4,8]],
+      [[7,5,4],[6,8,3],[1,' ',9]]],
+      [[[4,9,5],[3,7,6],[2,8,1]],
+      [[1,7,3],[9,2,8],[5,6,4]],
+      [[2,8,6],[1,4,5],[3,9,7]]],
+      [[[8,1,2],[7,5,4],[9,3,6]],
+      [[3,4,7],[' ',6,9],[8,1,5]],
+      [[5,6,9],[8,3,1],[4,7,2]]]]).
+
+      sudokuComplet([[[[9,2,8],[4,1,7],[6,5,3]],
+            [[6,3,1],[5,9,2],[7,4,8]],
+            [[7,5,4],[6,8,3],[1,2,9]]],
+            [[[4,9,5],[3,7,6],[2,8,1]],
+            [[1,7,3],[9,2,8],[5,6,4]],
+            [[2,8,6],[1,4,5],[3,9,7]]],
+            [[[8,1,2],[7,5,4],[9,3,6]],
+            [[3,4,7],[2,6,9],[8,1,5]],
+            [[5,6,9],[8,3,1],[4,7,2]]]]).
+
+non(A) :- A, !, fail.
+non(_).
+
+% appartenance à une liste
+element(_,[]):- fail.
+element(X,[X|_]):- !.
+element(X,[_|Q]):- element(X,Q).
+
+% is list?
+isList([]).
+isList([_|B]):- isList(B).
+
+% concatenation de listes
+concat([],C,C).
+concat([A|B],C,[A|E]):- concat(B,C,E),!.
+
+  %% ===AFFICHAGE SUDOKU=== %%
+
+% afficher liste de Trois elements
+affT([A|[]]) :- write(A), !.
+affT([A|B])  :- write(A), write(' '), affT(B).
+
+% afficher Ligne de listes
+affL([A|[]]) :- affT(A), !.
+affL([A|B])  :- affT(A), write('|'), affL(B).
+
+% afficher Groupe de lignes
+affG([A|[]]) :- affL(A), nl, !.
+affG([A|B])  :- affL(A), nl, affG(B).
+
+% afficher Sudoku
+affS([A|[]]) :- affG(A), !.
+affS([A|B])  :- affG(A), write('-----------------'), nl, affS(B).
+
+  %% ===MODIFICATION DES VALEURS=== %%
+
+%remplacer l'Yeme element de la ligne est remplace par V
+changer2(V,0,[_|B],[V|B]).
+changer2(V,Y,[A|B],[C|D]):- Y>0, Y1 is (Y-1), changer2(V,Y1,B,D), C = A.
+changer1(V,Y,[A|B],[C|D]):- Y<3, changer2(V,Y,A,C), D = B, !.
+changer1(V,Y,[A|B],[C|D]):- Y1 is (Y-3), changer1(V,Y1,B,D), C = A.
+
+%remplacer l'element (X,Y) par V: utiliser seulement changer(Valeur,Xcoor,Ycoor,InList,OutList)
+changer3(V,0,Y,[A|B],[C|B]):- changer1(V,Y,A,C).
+changer3(V,X,Y,[A|B],[C|D]):- X>0, X1 is (X-1), changer3(V,X1,Y,B,D), C = A.
+changer(V,X,Y,[A|B],[C|D]):- X<3, changer3(V,X,Y,A,C), D = B, !.
+changer(V,X,Y,[A|B],[C|D]):- X1 is (X-3), changer(V,X1,Y,B,D), C = A.
+
+% verifier que les coordonnées entrées par l'utilisateur sont valides 1-9
+validUserInputNumber(X):- X>0, X=<9, integer(X), !.
+validUserInputNumber(_):- nl, write('Saisie incorrecte !'),nl,nl, fail.
+
+% verifier que les coordonnées sont valides en interne 0-8
+validCoord(X):- X>=0, X=<8, integer(X), !.
+validCoord(_):- fail.
+
+  %% ===VERIFICATION SUDOKU=== %%
+
+% recuperer Xeme element (valeur ou liste) de la liste
+getNlist([A|_],0,A).
+getNlist([_|B],X,N) :- X>=1, X1 is X-1, getNlist(B,X1,N).
+
+% recuperer les 9 elements d'un bloc dans une liste
+getBlock(-1,_,_,[]):- !.
+getBlock(X,Y,[A|B],O):- X<0, X1 is (X+1), getNlist(A,Y,O1), getBlock(X1,Y,B,O2), concat(O1,O2,O).
+getBlock(X,Y,I,O):- getNlist(I,X,I1), X1 is -4, getBlock(X1,Y,I1,O), !.
+
+% recuperer les 9 elements d'une ligne dans une liste (Y coordonnée ligne)
+getLine(-1,_,[]):- !.
+getLine(Y,I,O):- Y<0, S is (Y+4), Y1 is Y+1, getNlist(I,S,O1), getLine(Y1,I,O2), concat(O1,O2,O).
+getLine(Y,I,O):- S is floor(Y/3), S1 is (Y rem 3), Y1 is -4, getNlist(I,S,I1), getNlist(I1,S1,I2), getLine(Y1,I2,O), !.
+
+% recuperer les 9 elements d'une colonne dans une liste
+
+  % ---- Fonctions intermediaires ----
+rowBlockFromRow([Trow|_], RowBlockIndex, Res):- RowBlockIndex =:= 0,
+  Res = Trow, !.
+rowBlockFromRow([_|Qrow], RowBlockIndex, Res):- NewIndex is RowBlockIndex -1,
+  rowBlockFromRow(Qrow,NewIndex,Res).
+
+columnElementFromRow(ColumnIndex,Row, ElementResult):- RowBlockIndex is floor(ColumnIndex/3),
+  ColIndex is (ColumnIndex rem 3),
+  rowBlockFromRow(Row, RowBlockIndex, Res),
+  getNlist(Res,ColIndex,ElementResult).
+
+columnFromRowTriplet(_,[],[]).
+columnFromRowTriplet(Column,[T|Q], Res) :- columnElementFromRow(Column,T,ElementResult),
+  columnFromRowTriplet(Column,Q,ElementResult2),
+  concat([ElementResult],ElementResult2,Res).
+  % ---------------------------------------
+
+getColumn(_,[],[]).
+getColumn(Column,[T|Q],Res):- columnFromRowTriplet(Column,T,Res2),
+  getColumn(Column,Q,Res3),
+  concat(Res2,Res3,Res),!.
+
+% verification d'elements repetés dans une liste (le ' ' ne compte pas)
+verif([],[]).
+verif([' '|Q],E):- !, verif(Q,E).
+verif([X|Q],E):- verif(Q,E), element(X,E), !, fail.
+verif([X|Q],[X|E]):- verif(Q,E).
+
+% verification validité d'un ajout
+verification(X,Y,Sudoku):- getLine(X,Sudoku,Line),
+  getColumn(Y,Sudoku,Column),
+  BlocX is floor(X/3),
+  BlocY is floor(Y/3),
+  getBlock(BlocX,BlocY,Sudoku,Bloc),
+  verif(Line,_),
+  verif(Column,_),
+  verif(Bloc,_).
+verification(_,_,_):- write('Cet ajout est invalide.'),nl,nl,fail.
+
+  %% ===ETAT DU SUDOKU=== %%
+
+% Ajout d'un élèment par l'utilisateur dans liste des cases jouées
+addPlayerMove(Coord):- current_predicate(jeuxJoueur/1), !,
+  jeuxJoueur(X),
+  concat(X,[Coord],Res),
+  retract(jeuxJoueur(_)),
+  assertz(jeuxJoueur(Res)).
+addPlayerMove(Coord):- asserta(jeuxJoueur([Coord])).
+
+% Lorsque case est effacée
+deleteFromList(X,[T|Q],Output):- X = T, Output = Q.
+deleteFromList(X,[T|Q],Output):- X \= T,
+  deleteFromList(X,Q,RestOfList),
+  concat(T,RestOfList,Output).
+
+deletePlayerMove(Coord):- isJeuJoueur(Coord),
+  jeuxJoueur(X),
+  deleteFromList(Coord,X,Output),
+  retract(jeuxJoueur(_)),
+  assertz(jeuxJoueur(Output)).
+
+% Savoir si une coordonnée est jouée par un joueur
+isJeuJoueur(Coord):- current_predicate(jeuxJoueur/1),
+  jeuxJoueur(X),
+  element(Coord,X).
+
+  % ----Savoir si le sudoku est complet----
+
+% predicat intermediaire pr savoir si une liste est complete
+listComplete([]).
+listComplete([T|Q]):- (T\=' '),!,listComplete(Q).
+
+% sudoku complet
+isSudokuComplete(-1,_):-!.
+
+isSudokuComplete(Index,Sudoku):- getLine(Index,Sudoku,Line),
+  listComplete(Line),
+  NewIndex is Index-1,
+  isSudokuComplete(NewIndex,Sudoku).
+
+isSudokuComplete(Sudoku):- getLine(8,Sudoku,Line),
+  listComplete(Line),
+  isSudokuComplete(7,Sudoku).
+  % ---------------------------------------
+
+% recuperer l'element à une certaine coordonée
+getElement(X,Y,Sudoku,Element):- validCoord(X),
+  validCoord(Y),
+  getLine(X,Sudoku,Line),
+  getNlist(Line,Y,Element).
+
+% verifier si la case est une case jouée
+isPlayableCell(X,Y,Sudoku):- getElement(X,Y,Sudoku,Element), Element = ' ',!.
+isPlayableCell(X,Y,_):- isJeuJoueur([X,Y]),!.
+isPlayableCell(_,_,_):- write('Cette case n\'est pas jouable!'),nl,fail.
+
+% verifier si une case est complétée (à utiliser apres is playable isPlayableCell)
+
+isCellCompleted(X,Y,Sudoku):- getElement(X,Y,Sudoku,Element),
+                          Element \= ' ',!.
+isCellCompleted(_,_,_):- write('Cette case est deja vide.'),nl,fail.
+
+
+
+  %% ===RESOLUTION SUDOKU=== %%
+
+% Ajouter un numero random dans un sudoku ajouterRand(InSudoku,OutSudoku)
+ajouterRand(I,O1):- repeat, X is random(0,9), repeat , Y is random(0,9), getElement(X,Y,I,' '), repeat, N is random(0,9),
+  changer(X,Y,N,I,O), verificationInput(X,Y,O), O1=O.
+
+% resoudre un sudoku
+completerSudoku(I,O):- ajouterRand(I,O1), completerSudoku(O1,O).
+
+% Verifier si le programme doit finir
+isProgramFinished(Sudoku,_):- isSudokuComplete(Sudoku), write('Vous avez gagné !'),nl,!.
+isProgramFinished(_,Choice):- Choice = 4.
+
+  %% ===PROGRAMME PRINCIPAL=== %%
+
+sudoku :- nl,
+  write('====================================================='),nl,
+  write('======= Bienvenue dans notre programme sudoku ======='),nl,
+  write('====================================================='),nl,nl,
+  repeat, menu, !.
+menu :- write('\t\t=====  MENU  ====='),nl,nl,
+  write(' Que voulez vous faire ?'),nl,nl,
+  write('1. Resoudre un sudoku'),nl,
+  write('2. Proposer un sudoku'),nl,
+  write('4. Quitter'),nl,nl,
+  write('Que voulez-vous faire ? : '),
+  read(Choice), nl,
+  handle(Choice),
+  Choice=4, nl.
+
+handle(1):- write('---- RESOLUTION D\'UN SUDOKU ----'),nl,
+            sudokuTest(S),
+            asserta(sudokuGrid(S)),
+            repeat,
+            userSolvingSudoku, !.
+handle(2):- write('---- PROPOSER UN SUDOKU ----'), nl, !.
+handle(4):- write('---- Au revoir ! ----'),!.
+handle(_):- write('---- Option invalide ----'),!.
+
+userSolvingSudoku :- nl,write('---- Complétez le sudoku ----'),nl,nl,
+   sudokuGrid(S), affS(S), nl,
+  write('1. Definir numero'), nl,
+  write('2. Effacer numero'), nl,
+  write('4. Quitter'), nl, nl,
+  write('Que voulez-vous faire ? : '),
+  read(Choice), nl,
+  handleResolution(Choice,S),
+  sudokuGrid(ModifiedGrid),
+  isProgramFinished(ModifiedGrid,Choice).
+
+% Definir d'un numero
+handleResolution(1,S):- write('Ligne de la case à modifier : '), read(X), validUserInputNumber(X),nl,
+  write('Colonne de la case à modifier :'), read(Y), validUserInputNumber(Y),nl,
+  write('Valeur de la case : '), read(N), validUserInputNumber(N),nl,
+  X1 is (X-1), Y1 is (Y-1),
+  isPlayableCell(X1,Y1,S),
+  changer(N,X1,Y1,S,S1),
+  verification(X1,Y1,S1),
+  retract(sudokuGrid(S)),
+  asserta(sudokuGrid(S1)),
+  addPlayerMove([X1,Y1]),
+  write('Succès de l\'ajout.'),nl,!.
+
+handleResolution(1,_):- !.
+
+% Effacer un numero
+handleResolution(2,S):- write('Ligne de la case à effacer :'), read(X), validUserInputNumber(X),nl,
+  write('Colonne de la case à effacer:'), read(Y), validUserInputNumber(Y),nl,
+  X1 is (X-1), Y1 is (Y-1),
+  isPlayableCell(X1,Y1,S),
+  isCellCompleted(X1,Y1,S),
+  changer(' ',X1,Y1,S,S1),
+  retract(sudokuGrid(S)),
+  asserta(sudokuGrid(S1)),
+  deletePlayerMove([X,Y]),!.
+
+handleResolution(2,_):- !.
+
+handleResolution(4,_):-!.
+
+handleResolution(_,_):- nl, write('Option invalide'), nl, !, fail.
+
+
+  %% ---AUTRES2--- %%
+
+prtch(V,X,Y,I):- changer(V,X,Y,I,O), affS(O).
